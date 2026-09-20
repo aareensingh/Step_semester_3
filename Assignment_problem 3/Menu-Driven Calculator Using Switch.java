66a78e6a523a352c5044098adf64962f6695207b
aareensingh <aareensingh@gmail.com>
2026-09-20 14:05:19 +0530

    Implement menu-driven calculator using switch

---
+++ Assignment_problem 3/Menu-Driven Calculator Using Switch.java
@@ -0,0 +1,36 @@
+class Calculator {
+    void calculate(double a, double b, char operator) {
+        switch (operator) {
+            case '+':
+                System.out.println("Result: " + (a + b));
+                break;
+
+            case '-':
+                System.out.println("Result: " + (a - b));
+                break;
+
+            case '*':
+                System.out.println("Result: " + (a * b));
+                break;
+
+            case '/':
+                if (b == 0) {
+                    System.out.println("Cannot divide by zero");
+                } else {
+                    System.out.println("Result: " + (a / b));
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        c.calculate(10, 4, '+');
+        c.calculate(10, 0, '/');
+        c.calculate(10, 4, '%');
+    }
+}
