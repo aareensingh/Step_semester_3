c0212a0c108bc8ca360356d680fa523fb966b48c
aareensingh <aareensingh@gmail.com>
2026-09-20 13:59:28 +0530

    Add Java program to sum natural numbers using while loop

---
+++ Assignment_problem 3/Sum of Natural Numbers Using a While Loop.java
@@ -0,0 +1,19 @@
+class NaturalNumberSum {
+    void sumOfNaturalNumbers(int n) {
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n) {
+            sum += i;
+            i++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+
+    public static void main(String[] args) {
+        NaturalNumberSum s = new NaturalNumberSum();
+
+        s.sumOfNaturalNumbers(5);
+    }
+}
