ed3c6f64f5c16da34c2952a09b70d3b052416f4e
aareensingh <aareensingh@gmail.com>
2026-09-20 13:58:40 +0530

    Add NumberPrinter class to print numbers up to N

--- Assignment_problem 3/Print Numbers 1 to N.java
+++ Assignment_problem 3/Print Numbers 1 to N.java
@@ -1,1 +1,13 @@
+class NumberPrinter {
+    void printNumbersUpToN(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
 
+    public static void main(String[] args) {
+        NumberPrinter p = new NumberPrinter();
+
+        p.printNumbersUpToN(5);
+    }
+}
