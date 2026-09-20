15471e5d67c7192efd34369b043c4d0da9c01e9d
aareensingh <aareensingh@gmail.com>
2026-09-20 14:01:05 +0530

    Add DayName class to print day names from numbers
    Implemented a class to print day names based on a number input.

---
+++ Assignment_problem 3/Day Name From Number.java
@@ -0,0 +1,36 @@
+class DayName {
+    void printDayName(int dayNumber) {
+        switch (dayNumber) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day number");
+        }
+    }
+
+    public static void main(String[] args) {
+        DayName d = new DayName();
+
+        d.printDayName(3);
+        d.printDayName(9);
+    }
+}
