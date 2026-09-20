d317e370537a5c513f4015c18cde0b4ca8acfccc
aareensingh <aareensingh@gmail.com>
2026-09-20 11:02:47 +0530

    Add word frequency report with stop-word filtering

---
+++ Assignment_problem 2/Stop-Word-Filtered Word Frequency Report.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class WordFrequencyReport
+{
+    static void printFilteredWordFrequency(String feedback)
+    {
+        String[] stopWords = {"the", "was", "and", "a", "is", "of", "in"};
+
+        feedback = feedback.toLowerCase();
+        feedback = feedback.replace(".", "").replace(",", "");
+
+        String[] words = feedback.split("\\s+");
+
+        HashMap<String, Integer> frequency = new HashMap<>();
+
+        for(int i = 0; i < words.length; i++)
+        {
+            boolean isStopWord = false;
+
+            for(int j = 0; j < stopWords.length; j++)
+            {
+                if(words[i].equals(stopWords[j]))
+                {
+                    isStopWord = true;
+                    break;
+                }
+            }
+
+            if(!isStopWord)
+            {
+                frequency.put(words[i], frequency.getOrDefault(words[i], 0) + 1);
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(frequency.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        for(Map.Entry<String, Integer> entry : list)
+        {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter feedback: ");
+        String feedback = sc.nextLine();
+
+        printFilteredWordFrequency(feedback);
+
+        sc.close();
+    }
+}
