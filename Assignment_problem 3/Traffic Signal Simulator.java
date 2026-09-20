03b0dbf06670e0bfde87eb147d0f4db611013fe0
aareensingh <aareensingh@gmail.com>
2026-09-20 14:09:10 +0530

    Implement Traffic Signal Simulator with basic states

---
+++ Assignment_problem 3/Traffic Signal Simulator.java
@@ -0,0 +1,29 @@
+class TrafficSignal {
+    void simulateTrafficSignal(int cycles) {
+        int state = 0;
+
+        for (int i = 1; i <= cycles; i++) {
+            switch (state) {
+                case 0:
+                    System.out.println("Red - Stop");
+                    break;
+
+                case 1:
+                    System.out.println("Green - Go");
+                    break;
+
+                case 2:
+                    System.out.println("Yellow - Prepare to stop");
+                    break;
+            }
+
+            state = (state + 1) % 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        TrafficSignal t = new TrafficSignal();
+
+        t.simulateTrafficSignal(3);
+    }
+}
